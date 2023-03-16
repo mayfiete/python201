9a4d24fbd038ff3471a6100653d23b0e28e6e5cd
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-03-16 12:18:17 -0400

    feat: not file

---
+++ not.py
@@ -0,0 +1,24 @@
+
+
+my_thing = False
+
+if my_thing:
+    print("Thing was true")
+else:
+    print("This is not true")
+
+if my_thing != True:
+    print("Um, what")
+
+
+name = "Terry"
+if name not in ['Joe', 'Bob']:
+    print("Not in the club")
+
+
+name1 = "Bryan"
+name2 = "Bryan"
+if name1 is not name2:
+    print("Different")
+else:
+    print("Same")
