162fb960293b33f8f21c0802b5aca449571424b5
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2023-03-16 12:07:58 -0400

    feat: in_operator example

---
+++ in_operator.py
@@ -0,0 +1,27 @@
+
+names = ['Kalob', 'Jon', 'Gully']
+
+print('Kalob' in names)
+
+
+if 'Gully' in names:
+    print('Booyah')
+
+
+my_answer = "rock"
+
+options = ["rock", "paper", "scissors"]
+
+if my_answer in options:
+    print("Bam!")
+
+
+key = "name"
+
+person = {
+    "name": "Terry",
+    "profession": "IT"
+}
+
+if key in person:
+    print("Good work")
